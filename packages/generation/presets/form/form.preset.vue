5a7f09fd925edcdf1306eb12dfe3bc898abbd1e1
axeltoll <116816793+axeltoll@users.noreply.github.com>
2024-09-30 22:19:07 +0000

    WOrkspace link

--- packages/generation/presets/form/form.preset.vue
+++ packages/generation/presets/form/form.preset.vue
@@ -31,7 +31,7 @@
 
 <script setup lang="ts">
 // Explicitly reference the globals.d.ts to include custom window properties
-/// <reference path="app/src/globalsd.ts" />
+/// <reference path="/workspaces/Retired-Young/app/src/globalsd.ts" />
 
 import type { _GenerationFormControlConfig } from '@tok/generation/defineConfig';
 import { SlidePreset } from '@tok/generation/presets/slide';
